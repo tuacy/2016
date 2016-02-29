a830efd64a8f25e0069a9a4b4bae3ebbe0e67f15
wuyunxing <1007178106@qq.com>
2016-02-29 16:53:00 +0800

    add animation

---
+++ Tuacy/xml/src/main/java/com/tuacy/xml/animation/viewanimation/ViewAnimationActivity.java
@@ -0,0 +1,33 @@
+package com.tuacy.xml.animation.viewanimation;
+
+import android.os.Bundle;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import com.tuacy.common.base.activity.BaseActivity;
+import com.tuacy.xml.R;
+
+public class ViewAnimationActivity extends BaseActivity {
+
+	private ImageView mAlphaView;
+	private ImageView mScaleView;
+	private ImageView mTranslateView;
+	private ImageView mRotateView;
+	private ImageView mSetView;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_view_animation);
+		mAlphaView = findView(R.id.img_view_animation_alpha);
+		mScaleView = findView(R.id.img_view_animation_scale);
+		mTranslateView = findView(R.id.img_view_animation_translate);
+		mRotateView = findView(R.id.img_view_animation_rotate);
+		mSetView = findView(R.id.img_view_animation_set);
+		mAlphaView.startAnimation(AnimationUtils.loadAnimation(this, R.anim.anim_alpha));
+		mScaleView.startAnimation(AnimationUtils.loadAnimation(this, R.anim.anim_scale));
+		mTranslateView.startAnimation(AnimationUtils.loadAnimation(this, R.anim.anim_translate));
+		mRotateView.startAnimation(AnimationUtils.loadAnimation(this, R.anim.anim_rotate));
+		mSetView.startAnimation(AnimationUtils.loadAnimation(this, R.anim.anim_set));
+	}
+}
